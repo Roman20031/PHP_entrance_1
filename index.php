03fa7b6ff14b7d1c0e7f617efef6464ed0d0527f
Roman Demianenko <Roman.Demianenko@student.souepl.cz>
2025-03-28 11:36:12 +0100

    PHP_entrance_1

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+    <?php
+        $servername = "localhost";
+        $username = "username";
+        $password = "password";
+        $dbname = "db_for_PHP_first_attempt";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+        VALUES ('John', 'Doe', 'john@example.com')";
+
+        if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+        } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+    ?>
+    </main>
+</body>
+</html>
