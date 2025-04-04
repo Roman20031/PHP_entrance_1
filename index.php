1da4a24bb997ff344303b078c59c4185befc817e
Roman Demianenko <Roman.Demianenko@student.souepl.cz>
2025-04-04 11:34:17 +0200

    PHP_entrance_1

--- index.php
+++ index.php
@@ -8,28 +8,28 @@
 <body>
     <main>
     <?php
-        $servername = "localhost";
-        $username = "username";
-        $password = "password";
-        $dbname = "db_for_PHP_first_attempt";
-
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        }
-
-        $sql = "INSERT INTO MyGuests (firstname, lastname, email)
-        VALUES ('John', 'Doe', 'john@example.com')";
-
-        if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-        } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-
-        $conn->close();
+           $servername = "localhost";
+           $username = "username";
+           $password = "password";
+           $dbname = "db_for_PHP_first_attempt";
+   
+           // Create connection
+           $conn = new mysqli($servername, $username, $password, $dbname);
+           // Check connection
+           if ($conn->connect_error) {
+           die("Connection failed: " . $conn->connect_error);
+           }
+   
+           $sql = "INSERT INTO ticket_form_first_attempt(ID, email, platforms, region, subject, decription, state, date)
+           VALUES ('1','john@example.com', )";
+   
+           if ($conn->query($sql) === TRUE) {
+           echo "New record created successfully";
+           } else {
+           echo "Error: " . $sql . "<br>" . $conn->error;
+           }
+   
+           $conn->close();
     ?>
     </main>
 </body>
